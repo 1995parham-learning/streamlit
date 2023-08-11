ab9ce7a235f7e2c0cf52da3f0ea03aae683caee5
Parham Alvani <parham.alvani@gmail.com>
2023-08-11 16:21:46 -0400

    feat: add conditional flow

--- main.py
+++ main.py
@@ -1,3 +1,18 @@
 import streamlit as st
 
 st.title("Hello World")
+
+
+def display_name(name: str):
+    st.info(f"Hello {name}, I am really happy to see you here")
+
+
+name = st.text_input("Please enter your name")
+
+if not name:
+    st.error("no name entered")
+    # stop this run, please note that the main application is still running
+    # and we only stop this specific run.
+    st.stop()
+
+display_name(name)
